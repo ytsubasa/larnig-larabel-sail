9ac68b4865f000a9bf3b862a7e804f27800e83cc
Y.Tsubasa <125607203+ytsubasa@users.noreply.github.com>
2025-09-11 08:45:54 +0900

    登録処理まで完了

---
+++ resources/views/regist/complete.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <mata charset='utf-8'>
+        <title>登録完了画面</title>
+</head>
+<body>
+    {{ $user->name }}さんを登録しました
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home');
@@ -9,3 +12,12 @@ Route::get('/', function () {
 Route::get('/home', function(){
     return view('home');
 });
+
+
+Route::get('/register',[RegisterController::class,'create'])
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register',[RegisterController::class,'store'])
+    ->middleware('guest');
+   
