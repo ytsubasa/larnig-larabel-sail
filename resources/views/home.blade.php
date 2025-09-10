ab170e4513431899ce62b014d5112acdfebfb6f5
Y.Tsubasa <125607203+ytsubasa@users.noreply.github.com>
2025-09-11 07:03:18 +0900

    初期

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <mata charset='utf-8'>
+            <title>トップ画面</title>
+    </head>
+    <body>
+        <p>こんにちわ！</p>
+    </body>
+</html>
