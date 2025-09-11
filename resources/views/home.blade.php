7aa7af0d0b12168ea9cbf9b2e8adb90359ebcc3e
Y.Tsubasa <125607203+ytsubasa@users.noreply.github.com>
2025-09-11 14:38:17 +0900

    サイクル完了

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <mata charset='utf-8'>
-            <title>トップ画面</title>
-    </head>
-    <body>
-        <p>こんにちわ</p>
-    </body>
-</html>
+<htmle lang="ja">
+<head>
+    <mata charset='utf-8'>
+        <title>トップ画面</title>
+</head>
+<body>
+    <p>こんにちは！
+@if (Auth::check())
+    {{n\Auth::user()->name }}さん</p>
+    <p><anhref="/logout">ログアウト</a><br><a href="/register">会員登録</a></p>
+@else
+    ゲストさん</p>
+    <p><a href="/logout ">ログイン</a><br><a href="/register">会員登録</a></p>
+@endif
+</body>
+</htmle>
