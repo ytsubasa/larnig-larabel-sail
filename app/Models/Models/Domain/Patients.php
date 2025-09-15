193da4dfbcb17c29253f356fee360f1496451f50
Y.Tsubasa <125607203+ytsubasa@users.noreply.github.com>
2025-09-15 19:05:26 +0900

    9月15日

---
+++ app/Models/Models/Domain/Patients.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Models\Domain;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Patients extends Model
+{
+    //
+}
---
+++ app/Models/Patient/Use.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Patient;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Use extends Model
+{
+    //
+}
