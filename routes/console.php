b07e49c7afeb1cd040329ff32520a23cdd8c5070
Y.Tsubasa <125607203+ytsubasa@users.noreply.github.com>
2025-09-15 19:14:44 +0900

    artisanコマンド追加

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Artisan::command('hello:closure', function() {
+    $this->comment('待たせたな！！！');
+    return 0;
+})->describe('サンプルですよコマンド');
+
+Artisan::command('ok', function() {
+    $this->comment('OKですよ、よくできたね！');
+});
